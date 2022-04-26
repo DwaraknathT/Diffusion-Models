638a0d8778157eed36980ad97f2bab35bc452e67
DwaraknathT <dwarakasharma@gmail.com>
2022-04-26 12:39:06 +0530

    added scs layers

---
+++ diffusion/layers/__init__.py
@@ -0,0 +1,3 @@
+from .misc import MaxAbsPool2d
+from .cossim_conv import CosSimConv2d
+from .cosim_conv_transpose import CosSimConvTranspose2d
--- diffusion/models/__init__.py
+++ diffusion/models/__init__.py
@@ -1,2 +1,3 @@
 from .unet import Unet
+from .scs_unet import SCS_Unet
 from .registry import get_model
